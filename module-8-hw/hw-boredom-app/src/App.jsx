7cb8a904f225913be12a597c702cf3b649ca418c
ktrn-s <rynasuh@gmail.com>
2026-03-12 20:46:54 -0400

    Boredom App. API hw

---
+++ module-8-hw/hw-boredom-app/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+
+  const [showActivity, setShowActivity] = useState("");
+  const [showType, setShowType] = useState("");
+  const [participants, setParticipants] = useState("");
+  
+  const getRes = async () => {
+      const response = await fetch("https://bored.api.lewagon.com/api/activity/");
+      const data = await response.json();
+      console.log(data);
+      setShowActivity(data.activity)
+      setShowType(data.type)
+      setParticipants(data.participants)
+  };
+
+  useEffect(() => {
+    getRes();
+  }, []);
+
+  return (
+    <div className='container'> 
+      <h1 className='heading'>Bored? Let’s Fix That. 🤔</h1>
+
+      <div className='act-card'>
+        <h3>{showActivity}</h3>
+        
+        <div className="info">
+          <p><span>Type:</span> {showType}</p>
+          <p><span>Participants:</span> {participants}</p>
+      </div>
+        <button
+        className="btn"
+        onClick={getRes}>Suggest Something Else </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
