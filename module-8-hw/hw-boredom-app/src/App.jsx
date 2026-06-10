4024a2f53e2438980e2e2a8cab55cebfd106d998
ktrn-s <rynasuh@gmail.com>
2026-06-09 21:49:17 -0400

    Update

--- module-8-hw/hw-boredom-app/src/App.jsx
+++ module-8-hw/hw-boredom-app/src/App.jsx
@@ -33,7 +33,7 @@ function App() {
       </div>
         <button
         className="btn"
-        onClick={getRes}>Suggest Something Else Please!!</button>
+        onClick={getRes}>Suggest Something Else</button>
       </div>
     </div>
   )
